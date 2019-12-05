2c9d5a4b9aee6b15cdb7e6ff4630a09f19411fd3
max d <frappyd@gmail.com>
2019-12-05 13:37:38 -0500

    added location permissions in the start activity

--- app/src/main/java/com/example/matchup/StartActivity.java
+++ app/src/main/java/com/example/matchup/StartActivity.java
@@ -1,11 +1,19 @@
 package com.example.matchup;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -20,8 +28,9 @@ public class StartActivity extends AppCompatActivity {
     protected void onStart() {
         super.onStart();
 
-        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        checkLocationPermissions();
 
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
 
         //Checks if user is null
         if(firebaseUser != null){
@@ -54,4 +63,61 @@ public class StartActivity extends AppCompatActivity {
         });
 
     }
+
+    private final int COARSE_LOCATION_REQUEST_PERMISSION = 98;
+
+    boolean checkLocationPermissions(){
+        if(ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_COARSE_LOCATION)
+            != PackageManager.PERMISSION_GRANTED) {
+
+            if(ActivityCompat.shouldShowRequestPermissionRationale(
+                    this, Manifest.permission.ACCESS_COARSE_LOCATION)){
+                new AlertDialog.Builder(this)
+                        .setTitle("Location Permission")
+                        .setMessage("MatchUp requires your location data")
+                        .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                ActivityCompat.requestPermissions(StartActivity.this,
+                                        new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
+                                        COARSE_LOCATION_REQUEST_PERMISSION);
+                            }
+                        })
+                    .create()
+                    .show();
+            } else {
+                ActivityCompat.requestPermissions(StartActivity.this,
+                        new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
+                        COARSE_LOCATION_REQUEST_PERMISSION);
+            }
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case COARSE_LOCATION_REQUEST_PERMISSION: {
+                if(grantResults.length > 0 &&
+                    grantResults[0] == PackageManager.PERMISSION_GRANTED){
+
+                    /**
+                     * TODO: make use of location permission
+                     */
+                    Toast.makeText(this, "Location granted", Toast.LENGTH_LONG)
+                           .show();
+                } else {
+                    // force app to close
+                    Intent homeIntent = new Intent(Intent.ACTION_MAIN);
+                    homeIntent.addCategory(Intent.CATEGORY_HOME);
+                    homeIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    startActivity(homeIntent);
+                }
+            }
+        }
+    }
 }
+
+
