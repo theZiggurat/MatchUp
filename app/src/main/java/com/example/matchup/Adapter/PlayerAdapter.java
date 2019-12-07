b5c022493900d708899c3f061c04cd941462547e
AndreaDumalagan <ad1137@scarletmail.rutgers.edu>
2019-12-07 16:03:16 -0500

    Added User Online/Offline Status

--- app/src/main/java/com/example/matchup/Adapter/PlayerAdapter.java
+++ app/src/main/java/com/example/matchup/Adapter/PlayerAdapter.java
@@ -2,6 +2,7 @@ package com.example.matchup.Adapter;
 
 import android.content.Context;
 import android.content.Intent;
+import android.media.Image;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -23,10 +24,12 @@ public class PlayerAdapter extends RecyclerView.Adapter<PlayerAdapter.ViewHolder
 
     private Context mContext;
     private List<User> mUsers;
+    private boolean isChat;
 
-    public PlayerAdapter(Context mContext, List<User> mUsers){
+    public PlayerAdapter(Context mContext, List<User> mUsers, boolean isChat){
         this.mUsers = mUsers;
         this.mContext = mContext;
+        this.isChat = isChat;
     }
 
     @NonNull
@@ -45,6 +48,20 @@ public class PlayerAdapter extends RecyclerView.Adapter<PlayerAdapter.ViewHolder
             holder.profile_image.setImageResource(R.mipmap.ic_launcher);
         } else {
             Glide.with(mContext).load(user.getImageURL()).into(holder.profile_image);
+        }
+
+        if(isChat){
+            if(user.getStatus().equals("online")){
+                holder.img_online.setVisibility(View.VISIBLE);
+                holder.img_offline.setVisibility(View.GONE);
+            }
+             else {
+                holder.img_online.setVisibility(View.GONE);
+                holder.img_offline.setVisibility(View.VISIBLE);
+            }
+        } else {
+            holder.img_online.setVisibility(View.GONE);
+            holder.img_offline.setVisibility(View.GONE);
         }
 
         holder.itemView.setOnClickListener(new View.OnClickListener() {
@@ -67,12 +84,16 @@ public class PlayerAdapter extends RecyclerView.Adapter<PlayerAdapter.ViewHolder
 
         public TextView username;
         public ImageView profile_image;
+        private ImageView img_online;
+        private ImageView img_offline;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
 
             username = itemView.findViewById(R.id.username);
             profile_image = itemView.findViewById(R.id.profile_image);
+            img_online = itemView.findViewById(R.id.img_online);
+            img_offline = itemView.findViewById(R.id.img_offline);
         }
     }
 }
