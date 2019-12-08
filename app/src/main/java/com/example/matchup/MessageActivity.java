6cfb3e846c2d9b5c6c0f81228f0866e1c1cc70fc
AndreaDumalagan <ad1137@scarletmail.rutgers.edu>
2019-12-08 14:12:55 -0500

    Send notification Oreo +

--- app/src/main/java/com/example/matchup/MessageActivity.java
+++ app/src/main/java/com/example/matchup/MessageActivity.java
@@ -206,7 +206,7 @@ public class MessageActivity extends AppCompatActivity {
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 for(DataSnapshot snapshot : dataSnapshot.getChildren()){
                     Token token = snapshot.getValue(Token.class);
-                    Data data = new Data(fuser.getUid(), R.mipmap.ic_launcher, username+": "+message, "New Message",
+                    Data data = new Data(fuser.getUid(), R.mipmap.ic_launcher, username+": "+message, "MatchUp! New Message",
                             userid);
 
                     Sender sender = new Sender(data, token.getToken());
